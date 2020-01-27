4583fe85c2f56b8e0f8fd62df87592c94fa671da
Rahul D. Ghosal <rdghosal@gmail.com>
2020-01-26 21:43:59 -0500

    differentiated restcontroller/regcontroller

--- src/main/java/com/finance/demo/api/AppController.java
+++ src/main/java/com/finance/demo/api/AppController.java
@@ -1,15 +1,14 @@
 package com.finance.demo.api;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/")
-@RestController
-class AppContoller {
+@Controller
+public class AppController {
 
-    @GetMapping
-    public String testRoute() {
-        return "Good!";
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }
+
